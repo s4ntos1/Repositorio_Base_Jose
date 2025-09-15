87e19ac330c31e0effe679a80ef75acdb84dfa7f
JOSE B A SANTOS <josebenedito10223@gmail.com>
2025-09-15 11:35:16 -0300

    Add files via upload

---
+++ Modulo 2/Manipulação de Arquivos e Automação/tutu.py
@@ -0,0 +1,16 @@
+import csv 
+#lista = open("dados.csv")
+#arquivo = csv.DictReader(lista)
+#for alunos in arquivo:
+    # print(alunos['nome']) 
+
+
+#lista = open("dados.csv")
+#arquivo = csv.DictReader(lista)
+#for carros in arquivo:
+    #if carros ["Ano"] == "2020":
+       #print(carros)
+#print(alunos)
+#with open("dados.csv", "a" , newline="")as arquivo:
+    #escritor = csv.writer(arquivo)
+    #escritor.writerow (["Nissan", "Sky Line", "2012","30.000"])
