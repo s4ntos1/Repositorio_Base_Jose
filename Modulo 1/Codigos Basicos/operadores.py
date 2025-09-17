be86e1157a7d78ec9ccca70559852bfef0823a8b
JOSE B A SANTOS <josebenedito10223@gmail.com>
2025-09-17 10:34:27 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/operadores.py
@@ -0,0 +1,19 @@
+#numero1=int(input("qual numero vc quer:"))
+#numero2=int(input("qual numero vc quer:"))
+#resultado=numero1+numero2
+#print(resultado)
+
+#numero1=int(input("qual numero vc quer:"))
+#numero2=int(input("qual numero vc quer:"))
+#resultado=numero1/numero2
+#print(resultado)
+
+#numero1=int(input("qual numero vc quer:"))
+#numero2=int(input("qual numero vc quer:"))
+#resultado=numero1*numero2
+#print(resultado)
+
+numero1=int(input("qual numero vc quer:"))
+numero2=int(input("qual numero vc quer:"))
+resultado=numero1-numero2
+print(resultado)
